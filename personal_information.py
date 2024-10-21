50ebab179cdbd5124b334be0cbf4350f1ab1a8ad
Jomark <maguadjomarkglenn@gmail.com>
2024-10-21 23:09:14 +0800

    fixed something on peudocode

--- personal_information.py
+++ personal_information.py
@@ -1,4 +1,5 @@
 #Make an array using the dictionary format
+#Ask user input for name and age
 #Ask user for input inside a loop where they will be asked if they want to input another entry, which they could only answer with "Yes" and "No"
 #Ask user to input information again when, "Yes"
 #Find and print the oldest
