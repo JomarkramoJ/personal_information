689cae62f274d87a5593a82a796ac2c5c6d1d16b
Jomark <maguadjomarkglenn@gmail.com>
2024-10-21 17:58:34 +0800

    pseudocode

---
+++ personal_information.py
@@ -0,0 +1,4 @@
+#Make an array using the dictionary format
+#Ask user for input inside a loop where they will be asked if they want to input another entry, which they could only answer with "Yes" and "No"
+#Ask user to input information again when, "Yes"
+#Find and print the oldest
