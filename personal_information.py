2a9052bf69b0820864763bca6be674742327fef9
Jomark <maguadjomarkglenn@gmail.com>
2024-10-23 22:38:41 +0800

    Proper name, age adjustments and multiple oldest people

--- personal_information.py
+++ personal_information.py
@@ -1,23 +1,32 @@
 #Make an array using the dictionary format
 informations = {}
 
+def is_valid_name(input_string):
+    if any(char.isdigit() for char in input_string):
+        return False
+    return True
+
 #Ask user input for name and age
 while True:
-    input_name = input("Enter name: ")
-    if not input_name.isalpha():
-        print("Please enter a valid name")
-        continue
-
+    while True:
+        input_name = input("Enter name: ")
+        if is_valid_name(input_name):
+            break
+        else:
+            print("Invalid Name, Please don't include numbers")
     while True:
         try:
             input_age = int(input("Enter age: "))
-            if input_age >= 0:
+            if input_age >= 123 and input_age <= 130:
+                print("You now have the world record of being the oldest ever to live according to google")
+                break
+            elif input_age >= 0 and input_age <= 130:
                 break
             else:
                 print("invalid age")
         except:
             print("Enter a valid age ")
-            
+                
     informations[input_name] = input_age
 
 #Ask user for input inside a loop where they will be asked if they want to input another entry, which they could only answer with "Yes" and "No"
@@ -35,6 +44,11 @@ while True:
         break
 
 #Find and print the oldest
+
 if informations:
-    oldest = max(informations, key=informations.get)
-    print(f"The oldest is: {oldest} which is {input_age} years old")
+    oldest_age = max(informations.values())
+    oldest = [name for name, age in informations.items() if age == oldest_age]
+    if len(oldest) > 1:
+        print(f"The oldest people in the list are: {', '.join(oldest)}, all aged {oldest_age} years old.")
+    else:
+        print(f"The oldest is: {''.join(oldest)} which is {input_age} years old")
